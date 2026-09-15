1ce7116bdd19e2624a9704841c50f2db4df2d3da
Mehedi Hasan <alifmehedihasan44@gmail.com>
2026-09-15 22:02:23 +0600

    code

--- OOP_Theory/src/Teacher.java
+++ OOP_Theory/src/Teacher.java
@@ -1,4 +1,18 @@
 public class Teacher {
     String name,gender;
     int phone;
+
+    void setinfo(String n, String g, int p)
+    {
+        name = n;
+        gender = g;
+        phone = p;
+    }
+
+    void display()
+    {
+        System.out.println("Name = "+name);
+        System.out.println("Gender = "+gender);
+        System.out.println("Phone = "+phone);
+    }
 }
--- OOP_Theory/src/Test.java
+++ OOP_Theory/src/Test.java
@@ -2,24 +2,45 @@ public class Test {
     static void main() {
         Teacher teacher1 = new Teacher();
 
-        teacher1.name = "Mehedi Hasan";
-        teacher1.gender = "Male";
-        teacher1.phone = 1232434;
+        /* eivabeo info set kora jay
+
+            teacher1.name = "Mehedi Hasan";
+            teacher1.gender = "Male";
+            teacher1.phone = 1232434;
+         */
+
+        teacher1.setinfo("Mehedi Hasan","Male",1232434);
+        teacher1.display();
+
+        /* eivabeo print kora jay
 
         System.out.println("Name = "+teacher1.name);
         System.out.println("Gender = "+teacher1.gender);
         System.out.println("Phone = "+teacher1.phone);
 
+        */
+
         System.out.println();
+
 
         Teacher teacher2 = new Teacher();
 
-        teacher2.name = "Amit Hasan";
-        teacher2.gender = "Male";
-        teacher2.phone = 234322;
+        /* eivabeo info set kora jay
+            teacher2.name = "Amit Hasan";
+            teacher2.gender = "Male";
+            teacher2.phone = 234322;
+        */
+
+        teacher2.setinfo("Amit Hasan","Male",234322);
+        teacher2.display();
+
+        /* eivabeo print kora jay
 
         System.out.println("Name = "+teacher2.name);
         System.out.println("Gender = "+teacher2.gender);
         System.out.println("Phone = "+teacher2.phone);
+
+        */
     }
+
 }
